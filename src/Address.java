2045eb2a74ca82f55ddf7f80cf7a6ed6e800b869
Pranav Goyal <pr945005@dal.ca>
2024-01-04 13:43:25 +0530

    Added code for UC1 by creating Address class

---
+++ src/Address.java
@@ -0,0 +1,86 @@
+public class Address {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private int zip;
+    private int phNumber;
+    private String email;
+
+    public Address(String firstName, String lastName, String address, String city, int zip, int phNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.zip = zip;
+        this.phNumber = phNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public int getPhNumber() {
+        return phNumber;
+    }
+
+    public void setPhNumber(int phNumber) {
+        this.phNumber = phNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String toString() {
+        return "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", zip=" + zip +
+                ", phNumber=" + phNumber +
+                ", email='" + email + '\'';
+    }
+}
